ede48b479a078fdde87125b20b18fe6de42ba31e
Alex Tjokro <alex@tjok.ro>
2019-01-04 11:03:08 -0800

    Added npm browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	plumber = require('gulp-plumber'),
-	sourcemaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps'),
+	browserSync = require('browser-sync').create(); // will setup a static server and reload browser as we save files
 
 // ----------------------------------------
 // SCRIPT TASKS
@@ -15,12 +16,14 @@ var gulp = require('gulp'),
 // Build CSS/Sass/SCSS
 gulp.task('build-css', function() {
 	return gulp
-		.src('source/sass/**/*.scss')
+		.src(['node_modules/bootstrap/scss/bootstrap.scss',
+			  'source/sass/**/*.scss'])
 		.pipe(sourcemaps.init()) // Process the original sources
 		.pipe(plumber()) // Used to display error on Gulp Watch
 		.pipe(sass().on('error', sass.logError))
 		.pipe(sourcemaps.write()) // Add the map to modified source.
-		.pipe(gulp.dest('public/stylesheets/'));
+		.pipe(gulp.dest('public/stylesheets/'))
+		.pipe(browserSync.stream()) // notify the browser of changes
 });
 
 // ----------------------------------------
