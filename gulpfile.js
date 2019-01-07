90af7458ff5ded23f56276a3a0746ea69585ffdb
Alex Tjokro <alex@tjok.ro>
2019-01-07 10:36:29 -0800

    Completed build-js gulp task and added gulp default

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	concat = require('gulp-concat'),
 	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
 	browserSync = require('browser-sync').create(); // will setup a static server and reload browser as we save files
 
 // ----------------------------------------
@@ -30,7 +31,7 @@ gulp.task('build-css', function() {
 });
 
 //JS Hint task
-gulp.task('js-hint', function() {
+gulp.task('jshint', function() {
 	return gulp
 		.src('source/javascripts/**/*.js')
 		.pipe(jshint())
@@ -48,6 +49,9 @@ gulp.task('build-js', function() {
 		.pipe(plumber()) // Used to display error on Gulp Watch
 		.pipe(concat('bundle.js'))
 		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		//only uglify if gulp is ran with '--type production'
+		// .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
 		.pipe(sourcemaps.write()) // Add the map to modified source.
 		.pipe(gulp.dest('public/javascripts/'))
 		.pipe(browserSync.stream()); // notify the browser of changes
@@ -56,7 +60,7 @@ gulp.task('build-js', function() {
 // ----------------------------------------
 // WATCH TASKS
 // ----------------------------------------
-gulp.task('watch', ['build-css'], function() {
+gulp.task('watch', ['build-css', 'jshint', 'build-js'], function() {
 	browserSync.init({
 		server: './source'
 	});
@@ -64,3 +68,10 @@ gulp.task('watch', ['build-css'], function() {
 	gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'source/sass/**/*.scss'], ['build-css']);
 	gulp.watch('*.php').on('change', browserSync.reload);
 });
+
+// ----------------------------------------
+// DEFAULT TASKS
+// ----------------------------------------
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
