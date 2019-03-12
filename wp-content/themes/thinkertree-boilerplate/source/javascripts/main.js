b3cb823d6e86f28d4dc12ff5f4d6c2e8c5a8efe0
Alex Tjokro <alex@tjok.ro>
2019-03-12 00:42:30 -0700

    Added base main.js

---
+++ wp-content/themes/thinkertree-boilerplate/source/javascripts/main.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	function respond() {
+		//Put all responsive functions here
+		
+	}
+
+	$(document).ready(function() {
+		respond();
+	});
+
+	$(window).load(function() {
+
+	});
+
+	$(window).resize(function() {
+		respond();
+	});
+
+	$(window).scroll(function() {
+	});
+
+}(jQuery));
